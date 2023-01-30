9a57f6997ed2521fd9488852edc560baebdd9374
Kevin McGrath <kpmcgra@gmail.com>
2023-01-29 19:15:01 -0600

    contains function added

--- singlylinked.js
+++ singlylinked.js
@@ -31,19 +31,54 @@ class LinkedList {
     front(){
         return(this.head.data)
     }
+    sum() {
+        if (this.head == null) {
+            return(0);
+        }
+        let sum = 0;
+        let currentNode = this.head;
+        while (currentNode !== null) {
+            sum += currentNode.data;
+            currentNode = currentNode.next;
+        }
+        return(sum)
+    }
+    contains(value){
+        if(this.head.data == value){
+            return(true);
+        }
+        let currentNode = this.head;
+        while (currentNode !== null){
+            if(currentNode.data == value){
+                return(true)
+            }
+            currentNode = currentNode.next
+        }
+        return(false);
+    }
 }
-
 
 single = new LinkedList;
 single.addFront(18);
 console.log(single)
 single.addFront(5);
 console.log(single)
+console.log(single.contains(5));
+console.log(single.contains(18));
+console.log(single.contains(7));
 
 
-console.log(single.removeFront())
-single.addFront(67);
-console.log(single.front())
-
+// console.log(single.removeFront())
+// single.addFront(67);
+// console.log(single.front())
+// console.log(single.sum())
+// single.addFront(1);
+// console.log(single.sum())
+// for(var i = 0; i <= 99; i++){
+//     single.addFront(1);
+//     console.log(single.sum());
+// }
+// single2 = new LinkedList;
+// console.log(single2.sum());
 
 //method for removing the head. needs to set "head" to the next of the current head. head of next to null....""
